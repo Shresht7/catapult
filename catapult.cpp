a9d52b1dad5dd10e9f511cc3c12b37b969bb66ba
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-09 20:20:31 +0530

    Update comments

--- catapult.cpp
+++ catapult.cpp
@@ -1,5 +1,3 @@
-// catapult.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
 // Library
 #include <iostream>
 #include <fstream>
@@ -12,6 +10,10 @@
 #ifdef _WIN32
 	#include <Windows.h>
 #endif
+
+// ====
+// MAIN
+// ====
 
 int main(int argc, char *argv[])
 {
@@ -48,6 +50,10 @@ int main(int argc, char *argv[])
 	return 0;	// Exit successfully
 
 }
+
+// ----------------
+// HELPER FUNCTIONS
+// ----------------
 
 static std::vector<std::string> extractUrls(std::ifstream& file)
 {
@@ -87,14 +93,3 @@ static void openUrlInBrowser(std::string url)
 		std::cerr << "Cannot open URL on this operating system" << std::endl;
 	#endif
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
